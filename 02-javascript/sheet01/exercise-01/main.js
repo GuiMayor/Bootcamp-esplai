99edd439e693dc43c2dbe9eb9dfaaee0e4ef14ee
guille <villawolflobo@gmail.com>
2026-03-26 13:15:09 +0100

    feat(JavaScript):JavaScript-sheet01-exercise-01

---
+++ 02-javascript/sheet01/exercise-01/main.js
@@ -0,0 +1,11 @@
+const nombre = "Edward";
+const edad = 82;
+const ciudad = "Caracas"
+/*const experiencia = true;
+const tieneExperiencia = true;
+const noTieneExperiencia = false;
+console.log(experiencia && tieneExperiencia); */
+
+
+console.log(`Hola, me llamo ${nombre} y tengo ${edad} años y soy oriundo de ${ciudad} y me experiencia previa en programación es..
+`);
