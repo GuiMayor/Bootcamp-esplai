b77a2f703431e567b7c7af6621f1c4d74b5b8ea0
guille <villawolflobo@gmail.com>
2026-03-26 13:48:07 +0100

    feat(js)sheet01-exercise02-comprobacion-tipo

---
+++ 02-javascript/sheet01/exercise-02/main.js
@@ -0,0 +1,13 @@
+const nombre = "Edward";
+const edad = 82;
+const esMayorDeEdad = true
+let ciudad;
+const nombre2 = null
+
+
+
+console.log(`Valor: ${nombre}    | Tipo: ${typeof nombre}`);
+console.log(`Valor: ${edad}      | Tipo: ${typeof edad}`);
+console.log(`Valor: ${esMayorDeEdad}    | Tipo: ${typeof esMayorDeEdad}`);
+console.log(`Valor: ${ciudad} | Tipo: ${typeof ciudad}`);
+console.log(`Valor: ${nombre2}     | Tipo: ${typeof nombre2}`);
