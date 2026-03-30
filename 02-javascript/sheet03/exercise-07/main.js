fbc3a8918c1761448188134882201bde7c91cd8d
GuiMayor <villawolflobo@gmail.com>
2026-03-30 18:51:23 +0200

    feat(js):sheet03/listaPeliculas/exercise07

---
+++ 02-javascript/sheet03/exercise-07/main.js
@@ -0,0 +1,10 @@
+const peliculas = [ 
+{ id: 1, titulo: "Inception", anio: 2010, genero: "Ciencia ficción / Acción", disponible: "Netflix, Amazon Prime Video" }, 
+{ id: 2, titulo: "The Dark Knight", anio: 2008, genero: "Acción / Drama", disponible: "HBO Max, Amazon Prime Video" }, 
+{ id: 3, titulo: "Interstellar", anio: 2014, genero: "Ciencia ficción / Drama", disponible: "Netflix, Amazon Prime Video" } 
+]; 
+console.log(peliculas)
+console.log("------------------------")
+console.log(peliculas[1].titulo);
+console.log("------------------------")
+console.log(peliculas[2].disponible);
