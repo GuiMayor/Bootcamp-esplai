6836b5e44d684a8db730d8a0f12b21b705c87076
guille <villawolflobo@gmail.com>
2026-03-30 13:45:23 +0200

    feat(js):sheet03/exercise/01 al 05

---
+++ 02-javascript/sheet03/exercise-03/main.js
@@ -0,0 +1,36 @@
+/*. una lista de nombres de alumnos; 
+2. los datos de un producto; 
+3. varias tareas con título y estado;
+4. las ciudades visitadas por una persona; 
+5. un catálogo de películas con título, año y género. */
+
+const televisor = {
+marca:"LG",
+tamaño:40,
+precio: 20,
+stock: "40"
+};
+const alumnos = ["juan", "pedro","maria","daniel","mercedes","julia","carlos"];
+
+
+const tareas = [
+{titulo: "estudiar",estado: "realizado",},
+{titulo: "comer",estado: "realizado",},
+{titulo: "dormir",estado: "realizado",},
+{titulo: "deporte",estado: "no realizado",}
+];
+const peliculas = [
+{titulo: "el amor",año: 1994, género:"romance",},
+{titulo: "el unico",año: 1934, género:"documental",},
+{titulo: "el fenix",año: 2009, género:"acción",},
+{titulo: "cantinflas",año: 1956, género:"comedia",}
+];
+
+
+const ciudadesVisitadas = ["caracas","maracaibo", "amazonas","malaga", "madrid"];
+
+
+
+
+
+console.log(peliculas)
