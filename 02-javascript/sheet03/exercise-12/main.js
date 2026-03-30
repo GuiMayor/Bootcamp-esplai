e4914481eed18486c864b85c4fd71d7abf7d4376
GuiMayor <villawolflobo@gmail.com>
2026-03-31 00:07:10 +0200

    feat(jv)sheet03/spread/exercise12

---
+++ 02-javascript/sheet03/exercise-12/main.js
@@ -0,0 +1,12 @@
+const numeros = [1, 2, 3];
+console.log(numeros);
+
+const base = [ 1,2,3];
+const ampliado = [ ... base, 4, 5]; 
+console.log(ampliado);
+
+console.log("------------------");
+
+const original = [1, 2, 3];
+const copia = [...original];
+console.log(copia);
