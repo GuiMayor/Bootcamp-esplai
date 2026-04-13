f90b80e047a64583a8c6a392a3ce8083d8d4bd1b
guille <villawolflobo@gmail.com>
2026-04-13 12:55:33 +0200

    feat(react):Estructura/componente/WelcomeCard

---
+++ 03-react/ejercicios-react/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import WelcomeCard from './WelcomeCard'; 
+
+function App() {
+  return (
+    <div className="contenedor-global">
+      <h1>Mi proyecto de React</h1>
+      <WelcomeCard />    
+    </div>
+  );
+}
+
+export default App;
