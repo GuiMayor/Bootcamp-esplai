ac03d36cde8fc5c1579e036523163a3644529f3e
guille <villawolflobo@gmail.com>
2026-04-13 13:39:09 +0200

    feat(react):Corregido/EjercicioHero-EstructuraJSX

--- 03-react/ejercicios-react/src/App.jsx
+++ 03-react/ejercicios-react/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import WelcomeCard from './WelcomeCard'; 
+import Hero from './Hero';
 
 function App() {
   return (
     <div className="contenedor-global">
       <h1>Mi proyecto de React</h1>
-      <WelcomeCard />    
+      <WelcomeCard />  
+      <Hero/>  
     </div>
   );
 }
