cfd25b872dc2d8ecb3dbb9e7386322423db4e52f
GuiMayor <villawolflobo@gmail.com>
2026-04-18 16:00:49 +0200

    feat(react):Exercise07/ConstruirSecciones-react

---
+++ 03-react/ejercicios-react/src/components/Ejercicio07/CallToAction.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function CallToAction() {
+  return (
+    <section className="cta-block">
+      <h3>¿Listo para empezar tu carrera?</h3>
+      <button className="btn-primary">¡Inscríbete ahora!</button>
+    </section>
+  );
+}
+export default CallToAction;
