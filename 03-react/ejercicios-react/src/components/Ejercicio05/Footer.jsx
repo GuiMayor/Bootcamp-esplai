6d8be1385cfd92a5334e65b6630a3c24b8fb0953
GuiMayor <villawolflobo@gmail.com>
2026-04-18 13:26:18 +0200

    feat(react):ejercicio05/react

---
+++ 03-react/ejercicios-react/src/components/Ejercicio05/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function Footer() {
+  return (
+    <>
+      <p>continua aprendiendo</p>
+      <footer>Todos los derechos reservados</footer>
+    </>
+  );
+}
+
+export default Footer;
---
+++ 03-react/ejercicios-react/src/components/Ejercicio05/ModuleCard.jsx
@@ -0,0 +1,11 @@
+function ModuleCard() {
+  return (
+    <article className="card" style={{ border: '1px solid #ccc', padding: '15px', borderRadius: '8px' }}>
+      <h3>Fundamentos de React</h3>
+      <p>Aprenderemos a crear interfaces basadas en componentes y JSX funcional [1].</p>
+      <button>Saber más</button>
+    </article>
+  );
+}
+export default ModuleCard;
+
